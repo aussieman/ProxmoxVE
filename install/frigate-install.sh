da43077a1b9e9f8229aa503ff13accd2758150c1
Andrey Kondratyev <t250.net@gmail.com>
2025-09-05 18:35:28 +0300

    Update frigate-install.sh

--- install/frigate-install.sh
+++ install/frigate-install.sh
@@ -42,10 +42,10 @@ if [[ "$CTTYPE" == "0" ]]; then
 fi
 msg_ok "Set Up Hardware Acceleration"
 
-msg_info "Installing Frigate v0.16.0 (Perseverance)"
+msg_info "Installing Frigate v0.15.2 (Perseverance)"
 cd ~
 mkdir -p /opt/frigate/models
-curl -fsSL "https://github.com/blakeblackshear/frigate/archive/refs/tags/v0.16.0.tar.gz" -o "frigate.tar.gz"
+curl -fsSL "https://github.com/blakeblackshear/frigate/archive/refs/tags/v0.15.2.tar.gz" -o "frigate.tar.gz"
 tar -xzf frigate.tar.gz -C /opt/frigate --strip-components 1
 rm -rf frigate.tar.gz
 cd /opt/frigate
